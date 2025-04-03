847de2ab5531e4ec92767934260c2e7deedb4022
Bob Nicholson <bobniklv@gmail.com>
2025-04-03 20:46:30 +0300

    :sparkles: Add vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
     base: '',
+    server: {
+        host: '::',
+        port: 8080,
+    },
     plugins: [
         vue(),
         vueDevTools()
