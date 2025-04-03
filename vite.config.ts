7b325df6ef10845529164e76d9c077a3f5d01886
Bob Nicholson <bobniklv@gmail.com>
2025-04-03 20:44:38 +0300

    :truck: Migrate to vite

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from "vite-plugin-vue-devtools";
+
+export default defineConfig({
+    base: '',
+    plugins: [
+        vue(),
+        vueDevTools()
+    ]
+})
